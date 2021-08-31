1cf3fa78a8ffcd847e0647e2d8d2e8d4788bb3ff
Bram van der Velden <Bram_VanDerVelden@hotmail.com>
2021-08-31 20:27:19 +0200

    gestart met registratie waarvan ik eerst input checkers heb gemaakt

--- registratie/registratie.java
+++ registratie/registratie.java
@@ -1,2 +1,80 @@
-package registratie;public class registratie {
+package registratie;
+
+public class registratie{
+
+    public registratie(String username, String wachtwoord){
+
+        int isCorrect = 0;
+
+        if(inputChecker(username)){
+            isCorrect++;
+        }
+        if(checkStringSize(username)){
+            isCorrect++;
+        }
+        if(wachtwoordChecker()){
+            isCorrect++;
+        }
+
+        if(isCorrect == 3){
+            database.database.
+        }
+
+    }
+
+
+    public boolean inputChecker(String input){
+        String string = input;
+        String specialCharacters = "<>[]{}*^/.,';][:\"";
+        for (int i = 0; i < string.length(); i++)
+        {
+            char check = string.charAt(i);
+            if(specialCharacters.contains(Character.toString(check))) {
+                break;
+            }
+            else if(i == string.length()-1){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkStringSize(String input){
+        if (input.length() < 21){
+            return true;
+        }
+        else{
+            System.out.println("Je Inlognaam is groter dan 21 tekens");
+            return false;
+        }
+    }
+
+    public boolean wachtwoordChecker(String wachtwoord){
+        String string = wachtwoord;
+        boolean heeftCijfer = false;
+        boolean heeftHoofdletter = false;
+        boolean heeftkleineletter = false;
+
+        for(int i = 0; i < string.length(); i++){
+            char check = string.charAt(i);
+            if(Character.isDigit(check)){
+                heeftCijfer = true;
+            }
+            if(Character.isUpperCase(check)){
+                heeftHoofdletter = true;
+            }
+            if(Character.isLowerCase(check)){
+                heeftkleineletter = true;
+            }
+        }
+        if(heeftCijfer && heeftHoofdletter && heeftkleineletter){
+            return true;
+        }
+        System.out.println("Je moet minimaal 1 hoofletter, kleine letter en cijfer hebben in jouw wachtwoord!");
+        return false;
+    }
+
+
+
+
 }
