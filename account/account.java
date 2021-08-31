44cddf4e8157de71373963240cc3d375e181df8a
Bram van der Velden <Bram_VanDerVelden@hotmail.com>
2021-08-31 21:43:27 +0200

    ranking systeem gemaakt zodat een persoon meerdere subscripties en rankings heeft en account aangepast

--- account/account.java
+++ account/account.java
@@ -1,16 +1,22 @@
 package account;
 
+import rankingSysteem.ranking;
+
+import java.util.ArrayList;
+
 public class account {
     private String username;
     private String wachtwoord;
     private String email;
 
-    private boolean isEmailVerified;
+    private ArrayList<ranking> subscribedAccounts;
+
 
     public account(String username, String wachtwoord, String email) {
         this.username = username;
         this.wachtwoord = wachtwoord;
         this.email = email;
+        this. subscribedAccounts = new ArrayList<>();
     }
 
     public String getUsername() {
@@ -24,4 +30,12 @@ public class account {
     public String getEmail() {
         return email;
     }
+
+    public ArrayList<ranking> getSubscribedAccounts() {
+        return subscribedAccounts;
+    }
+    public void addSubscribedAccounts(ranking ranking){
+        subscribedAccounts.add(ranking);
+    }
+
 }
